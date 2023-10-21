9cde0df5295d63b5c7dd2a02b778aac284665387
Nguyen Thanh Hoai <21211tt3804@mail.tdc.edu.vn>
2023-10-21 11:06:19 +0700

    xong man hinh menu chuc nang admin

---
+++ app/src/main/java/com/example/quanlyquancaphe/activities/MenuChucNangAdminActicity.java
@@ -0,0 +1,14 @@
+package com.example.quanlyquancaphe.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuChucNangAdminActicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_chuc_nang_admin_acticity);
+    }
+}
