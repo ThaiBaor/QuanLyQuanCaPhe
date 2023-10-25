308774c00112a877ce11e0ec84fa7b4114254d6c
Nguyen Thanh Hoai <21211tt3804@mail.tdc.edu.vn>
2023-10-25 14:12:26 +0700

    xong man hinh quan ly kho, man hinh them nguyen lieu

---
+++ app/src/main/java/com/example/quanlyquancaphe/activities/ThemNguyenLieuActivity.java
@@ -0,0 +1,14 @@
+package com.example.quanlyquancaphe.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemNguyenLieuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.man_hinh_them_nguyen_lieu_activity);
+    }
+}
---
+++ app/src/main/java/com/example/quanlyquancaphe/adapters/NguyenLieuAdapter.java
@@ -0,0 +1,2 @@
+package com.example.quanlyquancaphe.adapters;public class NguyenLieuAdapter {
+}
