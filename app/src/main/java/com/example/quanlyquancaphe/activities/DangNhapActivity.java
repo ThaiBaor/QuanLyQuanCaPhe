cf456e47454a7b80f2b3295cade365720f0155d3
Nguyen Thanh Hoai <21211tt3804@mail.tdc.edu.vn>
2023-10-30 16:38:43 +0700

    xong menu chuc nang admin

---
+++ app/src/main/java/com/example/quanlyquancaphe/activities/DangNhapActivity.java
@@ -0,0 +1,14 @@
+package com.example.quanlyquancaphe.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DangNhapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manhinh_dangnhap_layout);
+    }
+}
