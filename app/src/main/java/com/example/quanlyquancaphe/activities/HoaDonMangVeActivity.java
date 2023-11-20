02248ae57cf78a8cd97186de5a844e3b21f7ccbf
nguyen-thanh-hoai <21211tt3804@mail.tdc.edu.vn>
2023-11-20 16:11:34 +0700

    them menu hoa don mang ve

--- app/src/main/java/com/example/quanlyquancaphe/activities/HoaDonMangVeActivity.java
+++ app/src/main/java/com/example/quanlyquancaphe/activities/HoaDonMangVeActivity.java
@@ -2,11 +2,16 @@ package com.example.quanlyquancaphe.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +21,7 @@ import com.example.quanlyquancaphe.models.Ban;
 import com.example.quanlyquancaphe.models.HoaDonMangVe;
 import com.example.quanlyquancaphe.models.HoaDonTaiBan;
 import com.example.quanlyquancaphe.models.Khu;
+import com.example.quanlyquancaphe.services.MenuSideBarThuNgan;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,7 +31,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class HoaDonMangVeActivity extends AppCompatActivity{
+public class HoaDonMangVeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
     ValueEventListener valueEventListener;
@@ -33,11 +39,20 @@ public class HoaDonMangVeActivity extends AppCompatActivity{
     HoaDonMangVeAdapter adapter;
     ArrayList<HoaDonMangVe> data = new ArrayList<>();
 
+    DrawerLayout drawerLayout;
+
+    Toolbar toolBar;
+
+    NavigationView navigationView;
+
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manhinh_hoadonmangve_layout);
         setControl();
+        setdrawer();
         loadData();
         adapter = new HoaDonMangVeAdapter(HoaDonMangVeActivity.this, data, new HoaDonMangVeAdapter.ItemClickListener() {
             @Override
@@ -60,12 +75,12 @@ public class HoaDonMangVeActivity extends AppCompatActivity{
 
     private void loadData() {
         firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference =firebaseDatabase.getReference().child("HoaDon");
+        databaseReference = firebaseDatabase.getReference().child("HoaDon");
         valueEventListener = databaseReference.child("MangVe").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 data.clear();
-                for (DataSnapshot item : snapshot.getChildren()){
+                for (DataSnapshot item : snapshot.getChildren()) {
                     HoaDonMangVe hoaDonMangVe = item.getValue(HoaDonMangVe.class);
                     if (!hoaDonMangVe.getDaThanhToan()) {
                         data.add(hoaDonMangVe);
@@ -83,5 +98,28 @@ public class HoaDonMangVeActivity extends AppCompatActivity{
 
     private void setControl() {
         recyclerView = findViewById(R.id.recyclerviewHDMV);
+        toolBar = findViewById(R.id.toolBar);
+        toolBar.setNavigationIcon(R.drawable.menu_icon);
+        toolBar.setTitle("Hóa đơn mang về");
+    }
+
+    private void setdrawer() {
+        drawerLayout = findViewById(R.id.nav_drawer_chucnang_thungan);
+        navigationView = findViewById(R.id.nav_view);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.open_nav, R.string.close_nav);
+        //setSupportActionBar(toolbar);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_thanhtoanmangdi);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        MenuSideBarThuNgan menuSideBarThuNgan = new MenuSideBarThuNgan();
+        menuSideBarThuNgan.chonManHinh(item.getItemId(), HoaDonMangVeActivity.this);
+        navigationView.setCheckedItem(item.getItemId());
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
